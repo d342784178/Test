dc4852bedea3e8fd7396c0e0ce72781b1ece9c38
DLJ <ljdong2@ifleweb.com>
2018-03-25 18:28:12 +0800

    lis问题

--- src/main/java/leetcode/dp/Lis.java
+++ src/main/java/leetcode/dp/Lis.java
@@ -3,6 +3,7 @@ package leetcode.dp;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -18,36 +19,55 @@ public class Lis {
 
     private static Map<Integer, List<Integer>> map = Maps.newHashMap();
 
-    //i,j表示数组长度
-    //d(i) = max{1, d(j)+1},其中j<i,array[j]<=array[i]
-    private List<Integer> getLonggest(int[] array, List<Integer> lists) {
-        if (lists == null) {
-            lists = Lists.newArrayList();
-        }
-        List<Integer> tempList = Lists.newArrayList();
-        tempList.addAll(lists);
-        int[] ints = new int[array.length - 1];
-        System.arraycopy(array, 0, ints, 0, array.length - 1);
-        if (lists.isEmpty() || array[array.length - 1] < lists.get(0)) {
-            tempList.add(0, array[array.length - 1]);
-        } else {
-            tempList.clear();
-            tempList.add(0, array[array.length - 1]);
-        }
 
-        if (tempList.size() != lists.size() + 1) {
-            map.put(lists.size(), lists);
-        }
-        if (array.length == 1) {
-            return lists;
+    /**
+     * i表示数组长度 lis表示最长非降子序列
+     * d(i) = int[i-1]>=int[i-2]? (lis[i-1].get(length-1)==int[i-2])?d(i-1)+1:(考虑子序列长度相等的情况)):d(i-i)
+     * @param next 用于新旧lis长度相等时 通过在newlis中加入i+1 来帮助判断
+     */
+    private List<Integer> getLonggest(int[] array, Integer next) {
+        int i1 = array[array.length - 1];
+        if (array.length - 1 > 0) {
+            int[] newArray = new int[array.length - 1];
+            System.arraycopy(array, 0, newArray, 0, array.length - 1);
+            int i2 = newArray[newArray.length - 1];
+            if (i1 >= i2) {
+                List<Integer> lis = getLonggest(newArray, i1);
+                if (lis.get(lis.size() - 1) == i2) {//lis[i-1].get(length-1)==int[i-2])
+                    lis.add(i1);
+                    return Lists.newArrayList(lis);
+                } else {
+                    //将i1,i2构成newlis 与lis比较长度
+                    ArrayList<Integer> newLis = Lists.newArrayList(i2);
+                    newLis.add(i1);
+                    if (newLis.size() == lis.size()) {//长度相等时 通过next来判断新旧lis的长度
+                        return next != null && next >= i1 ? newLis : lis;
+                    } else {
+                        return Lists.newArrayList(newLis.size() > lis.size() ? newLis : lis);
+                    }
+                }
+            } else {
+                return Lists.newArrayList(getLonggest(newArray, i1));
+            }
         } else {
-            return getLonggest(ints, tempList);
+            return Lists.newArrayList(array[0]);
         }
     }
 
-
     public static void main(String args[]) {
-        new Lis().getLonggest(new int[]{5, 3, 4, 8, 6, 7}, null);
-        System.out.println(map);
+        //1.返回4
+        System.out.println(new Lis().getLonggest(new int[]{4}, null));
+        //1.5>4 2.返回{4} 3,返回{4,5}
+        System.out.println(new Lis().getLonggest(new int[]{4, 5}, null));
+        //1.4<5 2.返回{4}
+        System.out.println(new Lis().getLonggest(new int[]{5, 4}, null));
+        //1.3<5 2.5>4 3.返回{4} 5.返回{4,5}
+        System.out.println(new Lis().getLonggest(new int[]{4, 5, 3}, null));
+        //1.7>6 2.6>3 3.3<5 4.5>4 5.返回{4} 6.返回{4,5} 7.{3.6}长度等于{4,5} 8.{3,6,7}长度>{4,5} 8.返回{3,6,7}
+        System.out.println(new Lis().getLonggest(new int[]{4, 5, 3, 6, 7}, null));
+
+
+
+
     }
 }
