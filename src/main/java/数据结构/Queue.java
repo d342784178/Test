f7d950bed39a4139c9699bdeb91af9cef25711c1
DLJ <ljdong2@ifleweb.com>
2018-03-28 20:08:12 +0800

    +队列实现

--- src/main/java/数据结构/Queue.java
+++ src/main/java/数据结构/Queue.java
@@ -3,48 +3,48 @@ package 数据结构;
 import org.junit.Assert;
 
 /**
- * Desc: 栈:链表实现
+ * Desc: 队列
  * Author: ljdong2
  * Date: 2018-03-28
- * Time: 19:44
+ * Time: 20:01
  */
-public class Stack<T> {
+public class Queue<T> {
     /**
      * 栈顶指针
      */
+    private Node<T> firstNode;
     private Node<T> lastNode;
 
     public T pop() {
-        Node<T> node = lastNode;
-        lastNode = lastNode.next;
+        Node<T> node = firstNode;
+        firstNode = firstNode.next;
         return node.t;
     }
 
     public void push(T t) {
-        if (lastNode == null) {
-            lastNode = new Node<>(t);
+        if (firstNode == null) {
+            firstNode = new Node<>(t);
+            lastNode = firstNode;
         } else {
-            Node<T> tNode = new Node<>(t);
-            tNode.next = lastNode;
-            lastNode = tNode;
+            lastNode.next = new Node<>(t);
+            lastNode = lastNode.next;
         }
     }
 
     public boolean isEmpty() {
-        return lastNode == null;
+        return firstNode == null;
     }
 
-
     public static void main(String args[]) {
-        Stack<Integer> stack = new Stack<>();
+        Queue<Integer> stack = new Queue<>();
         stack.push(1);
         Assert.assertTrue(stack.pop() == 1);
         stack.push(1);
         stack.push(2);
         stack.push(3);
-        Assert.assertTrue(stack.pop() == 3);
-        Assert.assertTrue(stack.pop() == 2);
         Assert.assertTrue(stack.pop() == 1);
+        Assert.assertTrue(stack.pop() == 2);
+        Assert.assertTrue(stack.pop() == 3);
         Assert.assertTrue(stack.isEmpty() == true);
     }
 }
