4dd40df8c9fb1bfdd4503e0b105b6f12b8bf19b1
DLJ <ljdong2@ifleweb.com>
2018-06-21 11:53:32 +0800

    es单元测试

---
+++ src/main/java/工具使用/es/EsTest.java
@@ -0,0 +1,483 @@
+package 工具使用.es;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.lucene.index.Terms;
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.DocWriteRequest;
+import org.elasticsearch.action.admin.cluster.node.tasks.get.GetTaskResponse;
+import org.elasticsearch.action.admin.cluster.node.tasks.list.ListTasksResponse;
+import org.elasticsearch.action.bulk.*;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.get.MultiGetItemResponse;
+import org.elasticsearch.action.get.MultiGetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.*;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.bytes.BytesArray;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.common.unit.ByteSizeUnit;
+import org.elasticsearch.common.unit.ByteSizeValue;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.reindex.*;
+import org.elasticsearch.script.Script;
+import org.elasticsearch.script.ScriptType;
+import org.elasticsearch.script.mustache.SearchTemplateRequestBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.histogram.DateHistogramInterval;
+import org.elasticsearch.search.aggregations.bucket.histogram.Histogram;
+import org.elasticsearch.search.aggregations.metrics.stats.Stats;
+import org.elasticsearch.search.aggregations.metrics.stats.StatsAggregationBuilder;
+import org.elasticsearch.search.sort.FieldSortBuilder;
+import org.elasticsearch.search.sort.SortOrder;
+import org.elasticsearch.tasks.TaskId;
+import org.elasticsearch.tasks.TaskInfo;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.net.InetAddress;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+import static org.elasticsearch.script.ScriptType.INLINE;
+
+/**
+ * Desc:
+ * Author: ljdong2
+ * Date: 2018-06-21
+ * Time: 09:46
+ */
+
+public class EsTest {
+
+    private TransportClient client;
+
+    @Before
+    public void setup() throws Exception {
+        // 设置集群名称
+        //配置详情 https://www.elastic.co/guide/en/elasticsearch/client/java-api/6.3/transport-client.html
+        Settings settings = Settings.builder().put("cluster.name", "myapplication-elk")
+                .put("client.transport.ignore_cluster_name", true).build();
+        // 创建client   tcp端口 !不是http端口
+        client = new PreBuiltTransportClient(settings)
+                .addTransportAddress(new TransportAddress(InetAddress.getByName("localhost"), 9300));
+    }
+
+    @Test
+    public void index() throws Exception {
+        //jsonbuilder方式
+        IndexResponse response = client.prepareIndex("twitter", "tweet", "1")
+                .setSource(jsonBuilder()
+                        .startObject()
+                        .field("user", "kimchy")
+                        .field("postDate", new Date())
+                        .field("message", "trying out Elasticsearch")
+                        .endObject())
+                .get();
+        //json字符串方式
+        String json = "{" +
+                "\"user\":\"kimchy\"," +
+                "\"postDate\":\"2013-01-30\"," +
+                "\"message\":\"trying out Elasticsearch\"" +
+                "}";
+        response = client.prepareIndex("twitter", "tweet")
+                .setSource(json, XContentType.JSON)
+                .get();
+        //map方式
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("user", "kimchy");
+        map.put("postDate", new Date());
+        map.put("message", "trying out Elasticsearch");
+        response = client.prepareIndex("twitter", "tweet")
+                .setSource(map)
+                .get();
+
+        // byte[]数组方式
+        ObjectMapper mapper    = new ObjectMapper(); // create once, reuse
+        byte[]       jsonBytes = mapper.writeValueAsBytes(json);
+        response = client.prepareIndex("twitter", "tweet")
+                .setSource(jsonBytes)
+                .get();
+    }
+
+    @Test
+    public void get() throws Exception {
+        GetResponse response = client.prepareGet("twitter", "tweet", "1").get();
+    }
+
+    @Test
+    public void delete() throws Exception {
+        DeleteResponse response = client.prepareDelete("twitter", "tweet", "1").get();
+    }
+
+    @Test
+    public void deleteByQuery() throws Exception {
+        //同步
+        BulkByScrollResponse response = DeleteByQueryAction.INSTANCE.newRequestBuilder(client)
+                .filter(QueryBuilders.matchQuery("gender", "male"))
+                .source("persons")
+                .get();
+        long deleted = response.getDeleted();
+
+        //异步callback
+        DeleteByQueryAction.INSTANCE.newRequestBuilder(client)
+                .filter(QueryBuilders.matchQuery("gender", "male"))
+                .source("persons")
+                .execute(new ActionListener<BulkByScrollResponse>() {
+                    @Override
+                    public void onResponse(BulkByScrollResponse response) {
+                        long deleted = response.getDeleted();
+                    }
+
+                    @Override
+                    public void onFailure(Exception e) {
+                        // Handle the exception
+                    }
+                });
+
+    }
+
+    /**
+     * 更新
+     */
+    @Test
+    public void update() throws Exception {
+        //全量更新 流式
+        UpdateRequest updateRequest = new UpdateRequest("index", "type", "1")
+                .doc(jsonBuilder()
+                        .startObject()
+                        .field("gender", "male")
+                        .endObject());
+        client.update(updateRequest).get();
+        //全量更新 链式
+        client.prepareUpdate("ttl", "doc", "1")
+                .setDoc(jsonBuilder()
+                        .startObject()
+                        .field("gender", "male")
+                        .endObject())
+                .get();
+
+        //局部更新
+        client.prepareUpdate("ttl", "doc", "1")
+                .setScript(new Script(INLINE, "ctx._source.gender = \"male\"", null, null))
+                .get();
+
+
+        //update or insert
+        IndexRequest indexRequest = new IndexRequest("index", "type", "1")
+                .source(jsonBuilder()
+                        .startObject()
+                        .field("name", "Joe Smith")
+                        .field("gender", "male")
+                        .endObject());
+        updateRequest = new UpdateRequest("index", "type", "1")
+                .doc(jsonBuilder()
+                        .startObject()
+                        .field("gender", "male")
+                        .endObject())
+                .upsert(indexRequest);
+        client.update(updateRequest).get();
+    }
+
+    @Test
+    public void updateByQuery() throws Exception {
+        UpdateByQueryRequestBuilder updateAction = UpdateByQueryAction.INSTANCE.newRequestBuilder
+                (client);
+        updateAction.filter(QueryBuilders.matchQuery("gender", "male"))
+                .size(1000)
+                .script(new Script(ScriptType.INLINE, "ctx._source.awesome = 'absolutely'", "painless", Collections
+                        .emptyMap()))
+                .abortOnVersionConflict(false)//防止因更新操作导致版本的冲突
+                .source()//对结果的操作
+                .addSort("cat", SortOrder.DESC);
+        BulkByScrollResponse bulkByScrollResponse = updateAction.get();
+
+        //查看所有update-by-query操作
+        ListTasksResponse tasksList = client.admin().cluster().prepareListTasks()
+                .setActions(UpdateByQueryAction.NAME).setDetailed(true).get();
+        for (TaskInfo info : tasksList.getTasks()) {
+            TaskId                  taskId = info.getTaskId();
+            BulkByScrollTask.Status status = (BulkByScrollTask.Status) info.getStatus();
+            // do stuff
+        }
+
+        //查看某个update-by-query操作
+        GetTaskResponse get = client.admin().cluster().prepareGetTask("taskId").get();
+
+        //取消所有update-by-query操作
+        client.admin().cluster().prepareCancelTasks().setActions(UpdateByQueryAction.NAME).get().getTasks();
+        //取消某个update-by-query操作
+        client.admin().cluster().prepareCancelTasks().setTaskId(new TaskId("", 1L)).get().getTasks();
+
+        //限流
+        RethrottleAction.INSTANCE.newRequestBuilder(client)
+                .setTaskId(new TaskId("", 1L))
+                .setRequestsPerSecond(2.0f)
+                .get();
+    }
+
+    /**
+     * 批量获取
+     */
+    @Test
+    public void multiGet() throws Exception {
+        MultiGetResponse multiGetItemResponses = client.prepareMultiGet()
+                .add("twitter", "tweet", "1")
+                .add("twitter", "tweet", "2", "3", "4")
+                .add("another", "type", "foo")
+                .get();
+
+        for (MultiGetItemResponse itemResponse : multiGetItemResponses) {
+            GetResponse response = itemResponse.getResponse();
+            if (response.isExists()) {
+                String json = response.getSourceAsString();
+            }
+        }
+    }
+
+    /**
+     * 批量操作
+     * 只允许一下几类操作
+     * @see DocWriteRequest.OpType
+     */
+    @Test
+    public void bulk() throws Exception {
+        BulkRequestBuilder bulkRequest = client.prepareBulk();
+        //加入index操作
+        bulkRequest.add(client.prepareIndex("twitter", "tweet", "1")
+                .setSource(jsonBuilder()
+                        .startObject()
+                        .field("user", "kimchy")
+                        .field("postDate", new Date())
+                        .field("message", "trying out Elasticsearch")
+                        .endObject()
+                )
+        );
+        //加入delete操作
+        bulkRequest.add(client.prepareDelete("twitter", "tweet", "2").request());
+
+        BulkResponse bulkResponse = bulkRequest.get();
+        if (bulkResponse.hasFailures()) {
+            for (BulkItemResponse bulkItemResponse : bulkResponse.getItems()) {
+                DocWriteRequest.OpType opType = bulkItemResponse.getOpType();
+            }
+        }
+    }
+
+    /**
+     * 批量操作监听
+     */
+    @Test
+    public void bulkProcessor() throws Exception {
+        //bulk监听器
+        BulkProcessor bulkProcessor = BulkProcessor.builder(
+                client,
+                new BulkProcessor.Listener() {
+                    @Override
+                    public void beforeBulk(long executionId,
+                                           BulkRequest request) {
+                    }
+
+                    @Override
+                    public void afterBulk(long executionId,
+                                          BulkRequest request,
+                                          BulkResponse response) {
+                    }
+
+                    @Override
+                    public void afterBulk(long executionId,
+                                          BulkRequest request,
+                                          Throwable failure) {
+                    }
+                })
+                .setBulkActions(10000)//每10000个操作 执行一次
+                .setBulkSize(new ByteSizeValue(5, ByteSizeUnit.MB))//每5m 执行一次
+                .setFlushInterval(TimeValue.timeValueSeconds(5))//每5s 执行一次
+                .setConcurrentRequests(1)//并发数量
+                .setBackoffPolicy(//设置backoff策略
+                        BackoffPolicy.exponentialBackoff(TimeValue.timeValueMillis(100), 3))
+                .build();
+
+        // 添加操作
+        bulkProcessor.add(client.prepareDelete("twitter", "tweet", "1").request());
+        bulkProcessor.add(client.prepareDelete("twitter", "tweet", "2").request());
+        // 强制刷入
+        bulkProcessor.flush();
+        // 关闭监听
+        bulkProcessor.close();
+        //等待关闭
+        //bulkProcessor.awaitClose(10, TimeUnit.MINUTES);
+
+        // Refresh your indices
+        client.admin().indices().prepareRefresh().get();
+        // Now you can start searching!
+        client.prepareSearch().get();
+    }
+
+
+    /**
+     * 重建索引
+     */
+    @Test
+    public void reIndex() throws Exception {
+        BulkByScrollResponse response = ReindexAction.INSTANCE.newRequestBuilder(client)
+                .destination("target_index")
+                .filter(QueryBuilders.matchQuery("category", "xzy"))
+                .get();
+    }
+
+    @Test
+    public void search() throws Exception {
+        SearchResponse response = client.prepareSearch("index1", "index2")
+                .setTypes("type1", "type2")
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(QueryBuilders.termQuery("multi", "test"))                 // Query
+                .setPostFilter(QueryBuilders.rangeQuery("age").from(12).to(18))     // Filter
+                .setFrom(0).setSize(60).setExplain(true)
+                .get();
+    }
+
+    @Test
+    public void multiSearch() throws Exception {
+        SearchRequestBuilder srb1 = client
+                .prepareSearch().setQuery(QueryBuilders.queryStringQuery("elasticsearch")).setSize(1);
+        SearchRequestBuilder srb2 = client
+                .prepareSearch().setQuery(QueryBuilders.matchQuery("name", "kimchy")).setSize(1);
+        MultiSearchResponse sr = client.prepareMultiSearch()
+                .add(srb1)
+                .add(srb2)
+                .get();
+
+        long nbHits = 0;
+        for (MultiSearchResponse.Item item : sr.getResponses()) {
+            SearchResponse response = item.getResponse();
+            nbHits += response.getHits().getTotalHits();
+        }
+    }
+
+    /**
+     * 类似redis的scan
+     */
+    @Test
+    public void scroll() throws Exception {
+        QueryBuilder qb = QueryBuilders.termQuery("multi", "test");
+
+        SearchResponse scrollResp = client.prepareSearch("index1")
+                .addSort(FieldSortBuilder.DOC_FIELD_NAME, SortOrder.ASC)
+                .setScroll(new TimeValue(60000))//在指定时间内执行scroll操作
+                .setQuery(qb)
+                .setSize(100)//每次最大返回书
+                .get();
+        do {
+            for (SearchHit hit : scrollResp.getHits().getHits()) {
+                //处理返回结果
+            }
+            scrollResp = client.prepareSearchScroll(scrollResp.getScrollId())
+                    .setScroll(new TimeValue(60000))//在指定时间内执行scroll操作
+                    .execute()
+                    .actionGet();
+        }
+        while (scrollResp.getHits().getHits().length != 0); // 当返回数量为0时停止
+    }
+
+
+    /**
+     * 聚合
+     */
+    @Test
+    public void aggregations() throws Exception {
+        SearchResponse sr = client.prepareSearch()
+                .setQuery(QueryBuilders.matchAllQuery())
+                .addAggregation(
+                        AggregationBuilders.terms("agg1").field("field")
+                )
+                .addAggregation(
+                        AggregationBuilders.dateHistogram("agg2")
+                                .field("birth")
+                                .dateHistogramInterval(DateHistogramInterval.YEAR)
+                )
+                .get();
+
+        Terms     agg1 = sr.getAggregations().get("agg1");
+        Histogram agg2 = sr.getAggregations().get("agg2");
+        //指标组合
+        sr = client.prepareSearch()
+                .addAggregation(
+                        AggregationBuilders.terms("by_country").field("country")
+                                .subAggregation(AggregationBuilders.dateHistogram("by_year")
+                                        .field("dateOfBirth")
+                                        .dateHistogramInterval(DateHistogramInterval.YEAR)
+                                        .subAggregation(AggregationBuilders.avg("avg_children").field("children"))
+                                )
+                )
+                .execute().actionGet();
+
+        //准备工作
+        StatsAggregationBuilder agg3 = AggregationBuilders.stats("agg1").field("egg");
+        //获取指标
+        Stats  agg   = sr.getAggregations().get("agg");
+        double min   = agg.getMin();
+        double max   = agg.getMax();
+        double avg   = agg.getAvg();
+        double sum   = agg.getSum();
+        long   count = agg.getCount();
+    }
+
+    /**
+     * template查询
+     */
+    @Test
+    public void searchTemplate() throws Exception {
+        //保存脚本到es集群中
+        client.admin().cluster()
+                .preparePutStoredScript()
+                .setId("template_gender")
+                .setContent(new BytesArray(
+                        "{\n" +
+                                "    \"query\" : {\n" +
+                                "        \"match\" : {\n" +
+                                "            \"gender\" : \"{{param_gender}}\"\n" +
+                                "        }\n" +
+                                "    }\n" +
+                                "}"), XContentType.valueOf("mustache"));
+
+        //调用STORED脚本
+        Map<String, Object> template_params = new HashMap<>();
+        template_params.put("param_gender", "male");
+        SearchResponse sr = new SearchTemplateRequestBuilder(client)
+                .setScript("template_gender")//脚本名称
+                .setScriptType(ScriptType.STORED)//SOTRED脚本
+                .setScriptParams(template_params)//脚本参数
+                .setRequest(new SearchRequest())//上下文
+                .get()
+                .getResponse();
+        //调用INLINE脚本
+        sr = new SearchTemplateRequestBuilder(client)
+                .setScript("{\n" +
+                        "        \"query\" : {\n" +
+                        "            \"match\" : {\n" +
+                        "                \"gender\" : \"{{param_gender}}\"\n" +
+                        "            }\n" +
+                        "        }\n" +
+                        "}")
+                .setScriptType(ScriptType.INLINE)//INLINE脚本
+                .setScriptParams(template_params)
+                .setRequest(new SearchRequest())
+                .get()
+                .getResponse();
+    }
+
+
+
+}
---
+++ src/main/java/工具使用/es/QueryDSL.java
@@ -0,0 +1,166 @@
+package 工具使用.es;
+
+import org.apache.lucene.search.join.ScoreMode;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.network.InetAddresses;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.index.query.functionscore.FunctionScoreQueryBuilder;
+import org.elasticsearch.join.query.JoinQueryBuilders;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.junit.Test;
+
+import java.net.InetSocketAddress;
+
+import static org.elasticsearch.index.query.QueryBuilders.*;
+import static org.elasticsearch.index.query.functionscore.ScoreFunctionBuilders.exponentialDecayFunction;
+import static org.elasticsearch.index.query.functionscore.ScoreFunctionBuilders.randomFunction;
+
+/**
+ * Desc:
+ * Author: ljdong2
+ * Date: 2018-06-21
+ * Time: 11:19
+ */
+public class QueryDSL {
+    /**
+     * 查询所有
+     * @exception Exception
+     */
+    @Test
+    public void matchAll() throws Exception {
+        matchAllQuery();
+    }
+
+    @Test
+    public void fullTextQuery() throws Exception {
+        //精确匹配(相等)
+        matchQuery("name", "value");
+        //多值匹配
+        multiMatchQuery("name", "value1", "value2");
+        //模糊匹配(包含)
+        commonTermsQuery("name",
+                "kimchy");
+        queryStringQuery("+kimchy -elasticsearch");
+        //类似queryString 区别在于错误语法不抛异常
+        simpleQueryStringQuery("+kimchy -elasticsearch");
+
+    }
+
+    @Test
+    public void termLevelQuery() throws Exception {
+        //包含查询
+        termQuery("name", "value");
+        //包含查询 or关系
+        termsQuery("name", "value1", "value2");
+        //范围查询
+        rangeQuery("name").from(10).to(20);
+        //存在查询
+        existsQuery("name");
+        //前缀查询
+        prefixQuery("name", "prefix");
+        //类似正则查询
+        wildcardQuery("user", "k?mch*");
+        //正则查询
+        regexpQuery("name.first", "s.*y");
+        //近似查询 如:value=123 匹配 value=133
+        fuzzyQuery("name", "like");
+        //类型查询
+        typeQuery("type");
+        idsQuery("type1", "type2").addIds("id1", "id2");
+
+    }
+
+    /**
+     * @link https://www.elastic.co/guide/en/elasticsearch/client/java-api/6.3/java-compound-queries
+     * .html#java-query-dsl-function-score-query
+     */
+    @Test
+    public void compoundQuery() throws Exception {
+
+        //TODO 不知道啥用
+        constantScoreQuery(
+                termQuery("name", "kimchy"))
+                .boost(2.0f);
+        //符合所有条件
+        boolQuery()
+                .must(termQuery("content", "test1"))
+                .must(termQuery("content", "test4"))
+                .mustNot(termQuery("content", "test2"))
+                .should(termQuery("content", "test3"))
+                .filter(termQuery("content", "test5"));
+        //符合一个条件即可
+        disMaxQuery()
+                .add(termQuery("name", "kimchy"))
+                .add(termQuery("name", "elasticsearch"))
+                .boost(1.2f)
+                .tieBreaker(0.7f);
+
+        //TODO 不知道啥用
+        FunctionScoreQueryBuilder.FilterFunctionBuilder[] functions = {
+                new FunctionScoreQueryBuilder.FilterFunctionBuilder(
+                        matchQuery("name", "kimchy"),
+                        randomFunction()),
+                new FunctionScoreQueryBuilder.FilterFunctionBuilder(
+                        exponentialDecayFunction("age", 0L, 1L))
+        };
+        functionScoreQuery(functions);
+
+        //TODO 不知道啥用
+        boostingQuery(
+                termQuery("name", "kimchy"),
+                termQuery("name", "dadoonet"))
+                .negativeBoost(0.2f);
+
+    }
+
+    @Test
+    public void joinQuery() throws Exception {
+        //TODO nestJoinQuery
+        nestedQuery(
+                "obj1",
+                boolQuery()
+                        .must(matchQuery("obj1.name", "blue"))
+                        .must(rangeQuery("obj1.count").gt(5)),
+                ScoreMode.Avg);
+        //TODO parentOrChildQuery
+        Settings        settings = Settings.builder().put("cluster.name", "elasticsearch").build();
+        TransportClient client   = new PreBuiltTransportClient(settings);//使用PreBuiltTransportClient
+        client.addTransportAddress(new TransportAddress(new InetSocketAddress(InetAddresses.forString("127.0.0.1"),
+                9300)));
+        JoinQueryBuilders.hasChildQuery(
+                "blog_tag",
+                termQuery("tag", "something"),
+                ScoreMode.None);
+        JoinQueryBuilders.hasParentQuery(
+                "blog",
+                termQuery("tag", "something"),
+                false);
+
+    }
+
+    /**
+     * @link https://www.elastic.co/guide/en/elasticsearch/client/java-api/6.3/java-geo-queries.html
+     */
+    @Test
+    public void geoQuery() throws Exception {
+
+    }
+
+    /**
+     * @link https://www.elastic.co/guide/en/elasticsearch/client/java-api/6.3/java-specialized-queries.html
+     */
+    @Test
+    public void specialQuery() throws Exception {
+
+    }
+
+    /**
+     * @link https://www.elastic.co/guide/en/elasticsearch/client/java-api/6.3/java-span-queries.html
+     */
+    @Test
+    public void spanQuery() throws Exception {
+
+    }
+
+}
