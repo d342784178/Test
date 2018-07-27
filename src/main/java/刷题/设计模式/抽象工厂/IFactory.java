0765b4e85a2916ead3118529f2459d4e5ac28fea
DLJ <ljdong2@ifleweb.com>
2018-07-27 11:06:30 +0800

    抽象工厂

--- src/main/java/刷题/设计模式/抽象工厂/IFactory.java
+++ src/main/java/刷题/设计模式/抽象工厂/IFactory.java
@@ -1,7 +1,7 @@
 package 刷题.设计模式.抽象工厂;
 
 /**
- * Desc:
+ * Desc: 适合横向拓展
  * Author: ljdong2
  * Date: 2018-07-27
  * Time: 10:58
--- src/main/java/刷题/设计模式/简单工厂/Factory.java
+++ src/main/java/刷题/设计模式/简单工厂/Factory.java
@@ -1,7 +1,7 @@
 package 刷题.设计模式.简单工厂;
 
 /**
- * Desc:
+ * Desc:  适合纵向拓展
  * Author: ljdong2
  * Date: 2018-07-27
  * Time: 10:54
