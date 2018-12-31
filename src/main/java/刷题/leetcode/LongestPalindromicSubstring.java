36d167253c8c6920d7ede788abfd97bad0da7db4
DLJ <ljdong2@ifleweb.com>
2018-12-31 17:57:14 +0800

    加注释

--- src/main/java/刷题/leetcode/LongestPalindromicSubstring.java
+++ src/main/java/刷题/leetcode/LongestPalindromicSubstring.java
@@ -4,49 +4,67 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * //TODO 从中间开始找
  * Desc: 找到的最长回型子串 如:abcba aba abcddcba
  * Author: DLJ
  * Date: 2017-02-14
  * Time: 11:39
  */
 public class LongestPalindromicSubstring {
-    //效率低
-    //先获取所有首尾相同字符的子序列 再判断是不是回型的
     public String longestPalindrome(String s) {
         if (s == null || s.equals("")) {
             return "";
         }
-        List<String> list = new ArrayList<>();
+        //遍历,得到所有回文串
+        List<String> lists = new ArrayList<>();
         for (int i = 0; i < s.length(); i++) {
-            for (int j = 0; j <= i; j++) {
-                if (s.charAt(i) == s.charAt(j) && j >= 0 && j <= i) {
-                    String substring = s.substring(j, i + 1);
-                    if (judge(substring)) {
-                        list.add(substring);
-                    }
-                }
-            }
+            lists.add(find(s, i));
         }
+
         String ss = "";
-        for (String s1 : list) {
-            if (judge(s1) && ss.length() < s1.length()) {
-                ss = s1;
+        for (String list : lists) {
+            if (ss.length() < list.length()) {
+                ss = list;
             }
         }
         return ss;
     }
 
-    public boolean judge(String s) {
-        boolean ok = true;
-        for (int i = 0; i < s.length() / 2; i++) {
-            if (s.charAt(i) != s.charAt(s.length() - i - 1)) {
-                ok = false;
-                break;
-            }
+    //使用dp求解
+    //从中间向两边检测是否为回文串
+    public String find(String s, int index) {
+        if (index + 1 >= s.length()) {
+            return String.valueOf(s.charAt(index));
         }
-        return ok;
+        char c1 = s.charAt(index + 1);
+        char c  = s.charAt(index);
+        //判断是否可能为偶数型回文串
+        if (c != c1) {
+            return aroundFind(s, index - 1, index + 1);
+        } else {
+            //可能即是偶数也是奇数
+            String s1 = aroundFind(s, index - 1, index + 1);
+            String s2 = aroundFind(s, index - 1, index + 2);
+            return s2.length() > s1.length() ? s2 : s1;
+        }
     }
+
+    /**
+     * 向两边检测
+     */
+    public String aroundFind(String s, int left, int right) {
+        //超出边界
+        if (left < 0 || right > s.length() - 1) {
+            return s.substring(left + 1, right);
+        }
+        char leftChar  = s.charAt(left);
+        char rightChar = s.charAt(right);
+        if (leftChar == rightChar) {//相等继续向两边洁厕
+            return aroundFind(s, left - 1, right + 1);
+        } else {//不相等
+            return s.substring(left + 1, right);
+        }
+    }
+
 
     public static void main(String args[]) {
         System.out.println(new LongestPalindromicSubstring().longestPalindrome
